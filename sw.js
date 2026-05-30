dc136c6c9aa68e87ca49355cdda0f68323507d49
asiamayy <pqgkc2b7v4@privaterelay.appleid.com>
2026-05-30 10:30:05 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// FlipFinder Service Worker
+const CACHE = 'flipfinder-v2';
+const ASSETS = ['/FlipFinder/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
